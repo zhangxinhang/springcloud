5c294d2b0e64518bd32ffc31e145be1ebd7f49ff
ams-zxh <zxh@e-teng.com.cn>
2018-01-31 17:54:26 +0800

    init

---
+++ serice-feign/src/main/java/com/ams/sericefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.ams.sericefeign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry " + name;
+    }
+}
